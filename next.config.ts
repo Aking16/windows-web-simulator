2fe38fadcd2e8480201686034071c74e51cbf97b
Amirhossein Amiri <81476541+Aking16@users.noreply.github.com>
2025-03-21 00:18:16 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,8 +3,8 @@ import type { NextConfig } from "next";
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig: NextConfig = {
-  basePath: isProd ? '/windows-web-simulator' : '',
-  assetPrefix: isProd ? '/windows-web-simulator/' : '',
+  basePath: isProd ? 'https://aking16.github.io/windows-web-simulator/' : '',
+  assetPrefix: isProd ? 'https://aking16.github.io/windows-web-simulator/' : '',
   output: "export",
   images: {
     unoptimized: true
