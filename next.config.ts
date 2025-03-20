3302957b4bbcb7cdab3393bf1f242a6e94bd9e27
Aking <www.aking016@gmail.com>
2025-03-20 21:35:49 +0330

    👌 IMPROVE: Nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
 import type { NextConfig } from "next";
 
+const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
+
 const nextConfig: NextConfig = {
-  images: {
-    unoptimized: true,
-  },
+  assetPrefix: isGitHubPages ? 'https://aking16.github.io/windows-web-simulator' : '',
+  output: "export"
 };
 
 export default nextConfig;
