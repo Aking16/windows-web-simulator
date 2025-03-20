17827d960b83ac56920287020ec41650796a4b96
Aking <www.aking016@gmail.com>
2025-03-20 22:17:14 +0330

    📦 NEW: Minor change to the config

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const isGitHubPages = process.env.DEPLOY_TARGET === 'github-pages';
 
 const nextConfig: NextConfig = {
-  assetPrefix: isGitHubPages ? 'https://aking16.github.io/windows-web-simulator' : 'https://aking16.github.io/windows-web-simulator',
+  assetPrefix: isGitHubPages ? 'https://aking16.github.io/windows-web-simulator' : '',
   output: "export"
 };
 
