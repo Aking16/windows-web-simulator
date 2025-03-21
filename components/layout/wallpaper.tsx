9a30b3c97ad009029e48b3e0b220837e46c3ba12
Aking <www.aking016@gmail.com>
2025-03-21 21:57:14 +0330

    🐛 FIX: Fixed wallpaper height and width

--- components/layout/wallpaper.tsx
+++ components/layout/wallpaper.tsx
@@ -4,7 +4,12 @@ import React from 'react';
 const Wallpaper = () => {
   return (
     <div className="fixed top-0 left-0 -z-50">
-      <Image width={1920} height={1080} src="./wallpapers/white-theme-1.webp" alt="white theme wallpaper" />
+      <Image
+        width={1920}
+        height={1080}
+        src="./wallpapers/white-theme-1.webp"
+        alt="white theme wallpaper"
+        className="w-screen h-screen" />
     </div>
   );
 };
