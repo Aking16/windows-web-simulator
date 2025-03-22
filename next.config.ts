a31a2b077cb131be37fad421c8d8c7e5754b4fa1
Aking <www.aking016@gmail.com>
2025-03-22 15:01:44 +0330

    👌 IMPROVE: Changed the nextjs config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,5 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
-
-const nextConfig: NextConfig = {
-  basePath: isProd ? 'https://aking16.github.io/windows-web-simulator/' : '',
-  assetPrefix: isProd ? 'https://aking16.github.io/windows-web-simulator/' : '',
-  output: "export",
-  images: {
-    unoptimized: true
-  }
-};
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
