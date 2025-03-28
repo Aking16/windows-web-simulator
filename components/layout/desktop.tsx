dd3b438ef1485b286183631962f923b6ef5f8976
Aking <www.aking016@gmail.com>
2025-03-28 17:13:49 +0330

    👌 IMPROVE: Minor desktop improvement

--- components/layout/desktop.tsx
+++ components/layout/desktop.tsx
@@ -2,7 +2,7 @@ import ShortcutButton from "@/components/custom-ui/shortcut-button";
 
 const Desktop = () => {
   return (
-    <div>
+    <div className="px-4 py-2">
       <ShortcutButton />
     </div>
   );
